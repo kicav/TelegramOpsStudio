d0778d6e40584d1079a70bd05c41ea916e574555
kicav <bamto010399@gmail.com>
2026-08-13 17:14:17 +0700

    Update configure_deploy_spec.py

--- scripts/configure_deploy_spec.py
+++ scripts/configure_deploy_spec.py
@@ -26,7 +26,7 @@ def configure(path: Path) -> None:
     parser.set(
         "nuitka",
         "extra_args",
-        "--quiet --noinclude-qt-translations --assume-yes-for-downloads",
+        "--quiet --noinclude-qt-translations --assume-yes-for-downloads --output-filename=TelegramOpsStudio.exe"
     )
 
     with path.open("w", encoding="utf-8") as handle:
